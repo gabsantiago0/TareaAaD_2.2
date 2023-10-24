9508d31da5ea8e7775ebe70aaefb62798e02c3c8
Gabriel <gabsantiagoalfonso@gmail.com>
2023-10-24 22:55:19 +0200

    1.2

--- src/Ejercicio/Main.java
+++ src/Ejercicio/Main.java
@@ -20,28 +20,64 @@ public class Main {
             switch (opcion){
                 case 1:
 
-                    System.out.println("Elige una opción para crear:\n" +
+                    System.out.println("Elige una opción para CREAR:\n" +
                             "1. GRANJERO\n" +
                             "2. CONSTRACCIÓN\n" +
                             "3. TRACTOR\n");
 
-                    int opcion2 = Integer.parseInt(sc.nextLine());
-                    switch (opcion2){
+                    opcion = Integer.parseInt(sc.nextLine());
+                    switch (opcion){
                         case 1:
+                            System.out.println("a");
                             break;
                         case 2:
+
                             break;
                         case 3:
+
                             break;
                     }
                     break;
 
                 case 2:
+                    System.out.println("Elige una opción para MODIFICAR:\n" +
+                            "1. GRANJERO\n" +
+                            "2. CONSTRACCIÓN\n" +
+                            "3. TRACTOR\n");
+
+                    opcion = Integer.parseInt(sc.nextLine());
+                    switch (opcion){
+                        case 1:
+                            System.out.println("b");
+                            break;
+                        case 2:
+
+                            break;
+                        case 3:
+
+                            break;
+                    }
 
                     break;
 
                 case 3:
+                    System.out.println("Elige una opción para MODIFICAR:\n" +
+                            "1. GRANJERO\n" +
+                            "2. CONSTRACCIÓN\n" +
+                            "3. TRACTOR\n");
 
+                    opcion = Integer.parseInt(sc.nextLine());
+                    switch (opcion){
+                        case 1:
+                            System.out.println("c");
+                            break;
+                        case 2:
+
+                            break;
+                        case 3:
+
+                            break;
+                    }
                     break;
 
                 case 4:
